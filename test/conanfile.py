ee9f904e81529e73e2b6613848bfebeabcf323d4
Lars Bilke <lars.bilke@ufz.de>
2018-01-02 22:23:02 +0100

    Fixed test.

--- test/conanfile.py
+++ test/conanfile.py
@@ -15,8 +15,8 @@ class DefaultNameConan(ConanFile):
 
     def build(self):
         cmake = CMake(self)
-        cmake.configure(source_dir=self.conanfile_directory, build_dir="./")
-        cmake.build()
+        self.run('cmake %s %s' % (self.conanfile_directory, cmake.command_line))
+        self.run("cmake --build . %s" % cmake.build_config)
 
     def test(self):
-        self.run(".%stest" %os.sep)
+      self.run("cd bin && .%stest " % os.sep)
