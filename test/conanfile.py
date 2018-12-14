331aba07a60d9c80843cc8d61d6e1607192aab20
Lars Bilke <bilke@users.noreply.github.com>
2018-12-14 21:00:42 +0100

    Adapted test to Conan 1.0

--- test/conanfile.py
+++ test/conanfile.py
@@ -15,8 +15,8 @@ class DefaultNameConan(ConanFile):
 
     def build(self):
         cmake = CMake(self)
-        self.run('cmake %s %s' % (self.conanfile_directory, cmake.command_line))
-        self.run("cmake --build . %s" % cmake.build_config)
+        cmake.configure()
+        cmake.build()
 
     def test(self):
       self.run("cd bin && .%stest " % os.sep)
